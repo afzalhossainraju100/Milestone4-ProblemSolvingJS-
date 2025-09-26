14671887eb74d133f0ea1ad8f813b66f3ec4afad
afzalhossainraju100 <raju23105341031@diu.edu.bd>
2025-09-26 19:05:02 +0600

    solve problem no 4

--- AssignmentProblem3.js
+++ AssignmentProblem3.js
@@ -5,28 +5,18 @@ function bestTeam(team1, team2){
     const score1= team1.foul + team1.cardY + team1.cardR;
     const score2= team2.foul + team2.cardY + team2.cardR;
     if(score1 < score2){
-        return team1.t_name;
+        return team1.name;
     }
     else if(score2 < score1){
-        return team2.t_name;
+        return team2.name;
     }
     else{
         return "Tie";
     }
 
 }
-const teamA={
-        t_name: "Team A",
-        foul:15,
-        cardY:4,
-        cardR:1,
-        }
-const teamB = {
-  t_name: "Team B",
-  foul: 10,
-  cardY: 6,
-  cardR: 2,
-};
+const teamA = { name: "Brazil", foul: 5, cardY: 1, cardR: 0 };
+const teamB = { name: "Argentina", foul: 7, cardY: 0, cardR: 0 };
 
 const result = bestTeam(teamA, teamB);
 console.log(result);
---
+++ AssignmentProblem4.js
@@ -0,0 +1,22 @@
+function isSame(arr1,arr2){
+    if(!Array.isArray(arr1) || !Array.isArray(arr2)){
+        return "Invalid input";
+    }
+    if(arr1.length !== arr2.length){
+        return false;
+    }
+    for(let i=0;i<arr1.length;i++){
+        if(arr1[i] !== arr2[i]){
+            return false;
+        }
+    }
+
+
+
+    return true;
+
+}
+const array1 = [1, 2, 3];
+const array2 = [1, 2, 3];
+
+console.log(isSame(array1,array2));
